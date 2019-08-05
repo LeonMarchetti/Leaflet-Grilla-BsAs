5b9f32cdf807c38126e752cdc5d56e19171443d3
Leonardo Marchetti <mleonardoa@gmail.com>
2019-08-05 13:52:32 -0300

    Arreglé la muestra del mapa sin muestras

--- server.R
+++ server.R
@@ -176,7 +176,7 @@ agrupar_muestras <- function(lomb.sp, map) {
     #   agrupadas en cada celda de la grilla y con un valor de densidad
     #   promedio calculado para cada celda.
     #
-    agg <- aggregate(x = lomb.sp["dens"],
+    agg <- aggregate(x = lomb.sp,
                      by = map,
                      FUN = mean)
 
